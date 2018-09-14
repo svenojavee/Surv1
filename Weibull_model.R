b0de7aec4d924be4a81c8036f3eca2e400201399
admin <admin@wl5s-254-73.unil.ch>
2018-09-14 11:11:45 +0200

    Changes in likelihood. Problem: likelihood is not marginal (integrated over the parameters subject to selection (betas)).

--- Weibull_model.R
+++ Weibull_model.R
@@ -6,12 +6,15 @@ library(invgamma)
 library(data.table)
 
 #Sample size 
-N <- 100
+N <- 10000
 
 #Generate some survival data using one covariate beta
 
 #Effect sizes (arbitrary)
-betas <- rnorm(50,0,1)
+betas <- rnorm(10,0,2)
+#Set some betas to 0
+betas[sample(1:length(betas),5)] <- 0
+
 
 alfa <- 5
 mu <- -10
@@ -55,6 +58,7 @@ f_alfa <- function(alfa,mu,beta){
          -kappa0*alfa)
 }
 
+
 #alfa has to be an element (not vector!)
 f_alfa_derivative <- function(alfa,mu,beta){
   return((alfa0+
@@ -90,20 +94,20 @@ f_alfa_derivative_vec <- function(alfa,mu,beta){
 
 ###############
 #Function takes also the index (which beta are we sampling here)
-f_beta <- function(beta,alfa,mu,ind){
+f_beta <- function(beta,alfa,mu,ind,gamma_vec){
   BETA_USE <- copy(BETA)
   BETA_USE[ind] <- beta #Replace the specific beta index
-  return(sum(data_set$failure*data_set[,1+ind]*beta-((data_set$time_in_research)**alfa)*exp(mu+as.matrix(data_set[,2:(length(betas)+1)]) %*% BETA_USE))-
+  return(sum(data_set$failure*data_set[,1+ind]*beta-((data_set$time_in_research)**alfa)*exp(mu+as.matrix(X[,gamma_vec == 1]) %*% BETA_USE[gamma_vec==1]))-
            0.5*(1/sigma2)*(beta-mu0[ind])**2)
 }
-f_beta_vec <- function(beta,alfa,mu,ind){
-  return(unlist(lapply(beta,function(x) f_beta(x,alfa,mu,ind))))
+f_beta_vec <- function(beta,alfa,mu,ind,gamma_vec){
+  return(unlist(lapply(beta,function(x) f_beta(x,alfa,mu,ind,gamma_vec))))
 }
 
-f_beta_derivative <- function(beta,alfa,mu,ind){
+f_beta_derivative <- function(beta,alfa,mu,ind,gamma_vec){
   BETA_USE <- copy(BETA)
   BETA_USE[ind] <- beta #Replace the specific beta index
-  return(sum(data_set$failure*data_set[,1+ind] - data_set$time_in_research**alfa*data_set[,1+ind] * exp(mu+as.matrix(data_set[,2:(length(betas)+1)]) %*% BETA_USE))
+  return(sum(data_set$failure*data_set[,1+ind] - data_set$time_in_research**alfa*data_set[,1+ind] * exp(mu+as.matrix(X[,gamma_vec == 1]) %*% BETA_USE[gamma_vec == 1]))
          -1/(sigma2)*(beta-mu0[ind]))
 }
 
@@ -117,8 +121,8 @@ f_beta_derivative2 <- function(beta,beta_use,alfa_use,mu_use,ind){
   return(-sum(data_set$y**alfa_use * exp(mu_use+as.matrix(data_set[,2:(length(betas)+1)]) %*% beta_use) * (alfa_use*data_set[,1+ind])**2)-(1/sigma2))
 }
 
-f_beta_derivative_vec <- function(beta,alfa,mu,ind){
-  return(unlist(lapply(beta,function(x) f_beta_derivative(x,alfa,mu,ind))))
+f_beta_derivative_vec <- function(beta,alfa,mu,ind,gamma_vec){
+  return(unlist(lapply(beta,function(x) f_beta_derivative(x,alfa,mu,ind,gamma_vec))))
 }
 
 ################
@@ -126,6 +130,7 @@ f_beta_derivative_vec <- function(beta,alfa,mu,ind){
 f_mu <- function(mu,alfa_use,beta_use){
   return(mu*sum(data_set$failure)-sum(exp(mu+as.matrix(data_set[,2:(length(betas)+1)]) %*% beta_use)*data_set$time_in_research**alfa_use)-(1/(2*sigma_mu))*mu**2)
 }
+
 f_mu_derivative <- function(mu,alfa_use,beta_use){
   return(sum(data_set$failure)-sum(exp(mu+as.matrix(data_set[,2:(length(betas)+1)]) %*% beta_use)*data_set$time_in_research**alfa_use)-(1/(sigma_mu))*mu)
 }
@@ -194,10 +199,10 @@ abscissae_setter <- function(alfa,beta,mu,sigma2){
   beta_matrix <- matrix(rep(NA,length(beta)*length(cover)),nrow=5)
   for(i in 1:length(beta_suggest)){
     beta_suggest[i] <- beta_mode(beta_suggest[i],i,alfa_suggest,mu_suggest,beta_suggest,0.001)
-    beta_matrix[,i] <- beta_suggest[i] * c(1,1,1,1,1) + 10*cover2*sigma2
+    beta_matrix[,i] <- beta_suggest[i] * c(1,1,1,1,1) + 10*cover2
   }
   alfa_suggest <- alfa_suggest * cover
-  mu_suggest <- mu_suggest * c(1,1,1,1,1) + 10*cover2*sigma2
+  mu_suggest <- mu_suggest * c(1,1,1,1,1) + 10*cover2
   return(list(alfa=alfa_suggest,mu=mu_suggest,beta=beta_matrix))
 }
 
@@ -232,17 +237,20 @@ sigma_mu <- 10 #Just fix some big value
 BETA <- rep(0,length(betas))
 alfa <- 5
 mu <- -10
-pi <- 0.1   #Needs to be thought about
+pi <- 0.5   #Needs to be thought about
 
 res_matrix <- matrix(rep(NA,(length(betas)+3)*n),ncol=length(betas)+3)
 abscissae_set <- abscissae_setter(alfa,BETA,mu,sigma2)
 t1 <- Sys.time()
 
+gamma_vec <- rep(1,length(BETA))
+
 #First values for BETA
 for(ind in 1:length(BETA)){
-  new_beta <- ars(1,function(w) f_beta_vec(w,alfa,mu,ind),function(w) f_beta_derivative_vec(w,alfa,mu,ind),x=abscissae_set$beta[,ind],m=5)
+  new_beta <- ars(1,function(w) f_beta_vec(w,alfa,mu,ind,gamma_vec),function(w) f_beta_derivative_vec(w,alfa,mu,ind,gamma_vec),x=abscissae_set$beta[,ind],m=5)
   BETA[ind] <- new_beta
 }
+
 
 for(i in 1:n){
   alfa <- ars(1,function(w) f_alfa_vec(w,mu,BETA),function(w) f_alfa_derivative_vec(w,mu,BETA),x=abscissae_set$alfa,m=5,lb=T,xlb=0)
@@ -251,28 +259,38 @@ for(i in 1:n){
   
   #Compare the likelihoods for spike and slab
   for(ind in 1:length(BETA)){
-    l_spike <- log(pi) - sum(exp(mu+(X[,-ind] %*% (BETA[-ind])))*(data_set$y ** alfa) ) - 1/(2*sigma2) * BETA[-ind] %*% BETA[-ind]
-    l_slab <- log(1-pi) + sum(data_set$failure*X[,ind] * BETA[ind] ) - sum(exp(mu+(X %*% (BETA)))*(data_set$y ** alfa) ) - 1/(2*sigma2) * BETA %*% BETA
     
-    pi <- 1/(exp(l_slab-l_spike)+1)
+    #Full likelihood is required (not just the proportionality)
+    l_spike <- log(1-pi) + sum(data_set$failure==1)*log(alfa) + sum(data_set$failure*(mu + X[,-ind] %*% BETA[-ind]) + data_set$failure *(alfa-1)*data_set$time_in_research - 
+                                                                      exp(mu + X[,-ind] %*% BETA[-ind])*data_set$time_in_research)  
     
-    BETA[ind] <- BETA[ind] * (pi>runif(1))
+    l_slab  <- log(pi) + sum(data_set$failure==1)*log(alfa) + sum(data_set$failure*(mu + X %*% BETA) + data_set$failure *(alfa-1)*data_set$time_in_research - 
+                                                                   exp(mu + X %*% BETA)*data_set$time_in_research)+
+      0.5*sum(gamma_vec)*log(1/sigma2) - (1/(2*sigma2) * sum(BETA*BETA) )
+    
+    gamma_vec[ind] <- (l_spike/(l_spike+l_slab))>runif(1)  # 
   }
   
-  
-  for(ind in 1:length(BETA)){
+  for(ind in (1:length(BETA))){
     #abscissae_set <- abscissae_setter(alfa,BETA,z)
     #print(ind)
-    if(BETA[ind] != 0){
-      new_beta <- ars(1,function(w) f_beta_vec(w,alfa,mu,ind),function(w) f_beta_derivative_vec(w,alfa,mu,ind),x=abscissae_set$beta[,ind],m=5)
+    if(gamma_vec[ind] == 1){
+      new_beta <- ars(1,function(w) f_beta_vec(w,alfa,mu,ind,gamma_vec),function(w) f_beta_derivative_vec(w,alfa,mu,ind,gamma_vec),x=abscissae_set$beta[,ind],m=5)
       BETA[ind] <- new_beta
+    }else{
+      BETA[ind] <- 0
     }
   }
   #Calculate new sigma2 value (using only the non 0)
-  sigma2 <- rinvgamma(1,shape=sigma_alfa+0.5*length(betas[BETA!=0]),scale=1/(0.5* sum(BETA[BETA!=0]**2)+sigma_beta))  
+  sigma2 <- rinvgamma(1,shape=sigma_alfa+0.5*sum(gamma_vec),scale=1/(0.5* sum((BETA[gamma_vec==1])**2)+sigma_beta))  
+
+  theta <- rnorm(50,0,1)
+  sigma_test <- rinvgamma(1,shape=sigma_alfa+0.5*sum(gamma_vec),scale=1/(0.5* sum((theta[gamma_vec==1])**2)+sigma_beta))  
+  
+  
   
   #Use abscissae finder only for some time
-  if(i<20){
+  if(i %% 50 ==0){
     #print(i)
     t2 <- Sys.time()
     abscissae_set <- abscissae_setter(alfa,BETA,mu,sigma2)
@@ -284,7 +302,10 @@ for(i in 1:n){
 }
 print(Sys.time()-t1)
 
-hist(res_matrix[900:1000,2])
+
+sum(res_matrix[500:1000,12]==0)
+
+hist(res_matrix[500:1000,1])
 
 View(res_matrix[900:1000,])
 
